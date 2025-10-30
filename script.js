e0301bb71cd31b9ed5c349037bc9365bae6634dc
daoppapa-cmd <daoppapa@gmail.com>
2025-10-31 01:13:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -315,7 +315,7 @@ function isInsideArea(lat, lon) {
  * ផ្ទុក AI Models ពី Folder '/Model'
  */
 async function loadAIModels() {
-    const MODEL_URL = './Model'; // ត្រូវប្រាកដថា Folder នេះមានពិត
+    const MODEL_URL = './model'; // ត្រូវប្រាកដថា Folder នេះមានពិត
     loadingText.textContent = 'កំពុងទាញយក AI Models...';
     try {
         await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
